268ddf4b12e8365740001d9b5564b2781820150d
ravigks <ravi@gksedutech.com>
2018-12-28 10:52:02 +0530

    build changes

---
+++ Edujinni/Controllers/AddSubjectController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Edujinni.Controllers
+{
+    public class AddSubjectController : Controller
+    {
+        // GET: AddSubject
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ActionResult AddSubjects()
+        {
+            return View();
+        }
+        public ActionResult AddSubjectView()
+        {
+            return View();
+        }
+    }
+}
