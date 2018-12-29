115a8799b1ec922731e1a1ea8c938500ce0652eb
ravigks <ravi@gksedutech.com>
2018-12-29 15:43:08 +0530

    AddEventsView and EventsView

---
+++ Edujinni/Controllers/EventsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Edujinni.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult EvetsView()
+        {
+            return View();
+        }
+        public ActionResult AddEventsView()
+        {
+            return View();
+        }
+    }
+}
