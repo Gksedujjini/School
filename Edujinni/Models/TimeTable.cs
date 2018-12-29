3de8d2d9f7eed080e0e3e7b032d1dbfbcb2b4186
SaikiranGKS <saikiran@gksedutech.com>
2018-12-29 16:19:03 +0530

    add time table completed

---
+++ Edujinni/Models/TimeTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Edujinni.Models
+{
+    public class TimeTable
+    {
+        public string selectday { get; set; }
+        public string selectsubject { get; set; }
+        public string  teachername { get; set; }
+        public string classs { get; set;}
+        public string section { get; set; }
+        public string starttime { get; set; }
+        public string endtime { get; set; }
+        public string date { get; set; }
+             
+    }
+}
