66d8288ac562a5f435ddec3a3a9369c7bd3c549e
SaikiranGKS <saikiran@gksedutech.com>
2019-01-08 10:26:21 +0530

    popups

---
+++ Edujinni/Controllers/PopupsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Edujinni.Controllers
+{
+    public class PopupsController : Controller
+    {
+        // GET: Popups
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ExamAdded()
+        {
+            return View();
+        }
+        public ActionResult TimeTable()
+        {
+            return View();
+        }
+        public ActionResult StudentAdded()
+        {
+            return View();
+        }
+        public ActionResult TeacherAdded()
+        {
+            return View();
+        }
+        public ActionResult Notification()
+        {
+            return View();
+        }
+    }
+}
