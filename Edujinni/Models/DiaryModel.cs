5963439f8104d3d613dd79982484e01d830685f7
ravigks <ravi@gksedutech.com>
2019-01-09 11:40:36 +0530

    changes

---
+++ Edujinni/Models/DiaryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Edujinni.Models
+{
+    public class DiaryModel
+    {
+        public string Class { get; set; }
+        public string Section { get; set; }
+        public DateTime Date { get; set; }
+        public string Subject { get; set; }
+        public string ClassRelatedTest { get; set; }
+        public string HomeWork { get; set; }
+    }
+}
