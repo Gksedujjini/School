358d25ed79e94a4bc666df3a05a3e5cf95739ccb
ravigks <ravi@gksedutech.com>
2019-01-11 11:34:34 +0530

    changes

--- Edujinni/Controllers/TeacherController.cs
+++ Edujinni/Controllers/TeacherController.cs
@@ -25,5 +25,9 @@ namespace Edujinni.Controllers
         {
             return View();
         }
+        public ActionResult TeacherDashboard()
+        {
+            return View();
+        }
     }
 }
